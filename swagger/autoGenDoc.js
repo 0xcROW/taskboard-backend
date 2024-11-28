82208a48564715e59f85865ca94a7e087f56a363
Matheus Silva <bastiaotreze@gmail.com>
2024-11-28 00:41:59 -0300

    Initial commit

---
+++ swagger/autoGenDoc.js
@@ -0,0 +1,37 @@
+const mongooseToSwagger = require('mongoose-to-swagger');
+// const EsquemaTarefa = require('../src/models/tarefa.js');
+// const EsquemaUsuario = require('../src/models/usuario.js');
+const swaggerAutogen = require('swagger-autogen')({
+    openapi: '3.0.0',
+    language: 'pt-BR',
+});
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['../index.js'];
+
+let doc = {
+    paths: {},
+    components: {
+        schemas: {
+            // Tarefa: mongooseToSwagger(EsquemaTarefa),
+            // Usuario: mongooseToSwagger(EsquemaUsuario),
+        },
+    },
+    info: {
+        title: 'API de Tarefas',
+        description: 'API para manipulação de tarefas',
+        version: '1.0.0',
+    },
+    servers: [{
+        url: 'http://localhost:3000',
+        description: 'Servidor Local'
+    }],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+};
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    if (process.env.NODE_ENV !== 'production'){
+        require('../index.js');
+    }
+});
